722256213e9c2076643232165a9cbe5dce20c0ee
xinyu <yu.life@qq.com>
2024-09-05 21:37:05 +0800

    2024/09/05 周四

--- STM32/code/0-0 实验/module/SD卡/User/main.c
+++ STM32/code/0-0 实验/module/SD卡/User/main.c
@@ -8,51 +8,52 @@
 #include "ff.h"
 
 
-extern char file_num;//选中的文件标号
-extern DIR start_dirs; //目录起点信息
+// extern char file_num;//选中的文件标号
+// extern DIR start_dirs; //目录起点信息
 extern vu8 SD_Card_Ready;//SD卡初始化成功标志
 FATFS fs;            
-FIL fsrc, fdst, file;      // file objects
+FIL fsrc;      // file objects
 FRESULT res;         // FatFs function common result code
 UINT br, bw;         // File R/W count	
 
-static const u8 txt_buf[10]={1,2,3,4,5,6,7,8,9,10};
-
 int main()
 {
-	/*u8 i=0;
+	u8 i=0;
 	u16 value=0;
 	float vol;
 	float pressure;
+	char buffer[100];
 	
 	SysTick_Init(72);
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);  //中断优先级分组分2组
-	USART1_Init(115200);
 	ADC0_Pressure_Init();
+	USART1_Init(115200);
 	
-	
-	*/
 	printf("start...");
 	
 	u8 ret = 1;	
 	ret = MSD_Init();
 	// 串口初始化
-	USART1_Init(115200);
 	SD_Card_Ready = ret;//ret=0 ； 证明Ready
-	printf("ret:%d\n", ret);
+	// 挂载
 	res = f_mount(0, &fs);//res=0 ； 证明OK
-	printf("res1:%d\n", res);
-	res = f_open(&fsrc,"MEng.txt", FA_CREATE_ALWAYS | FA_WRITE);//res=0 ； 证明OK
-	printf("res2:%d\n", res);
-	res = f_write(&fsrc, txt_buf, 10, &bw);//写入BMP首部//res=0 ； 证明OK
-	printf("res3:%d\n", res);
+	// 打开或者创建一个文件
+	// &fsrc指向空白file的指针
+	// Test.txt指向的文件名
+	// 访问模式和文件打开模式标志
+	res = f_open(&fsrc,"Test.txt", FA_CREATE_ALWAYS | FA_WRITE | FA_OPEN_ALWAYS);//res=0 ； 证明OK
+	
+	const TCHAR *start_text = "start writing!\r\n";
+	
+	f_puts(start_text, &fsrc);
+	
 	f_close(&fsrc);
+	
 	f_mount(0, NULL);
 	
 	while(1)
 	{	
 		
-		/*
 		i++;
 		
 		if(i%100==0)
@@ -63,9 +64,11 @@ int main()
 			printf("检测电压值为：%.2fV\r\n", vol);
 			pressure = Get_Voltage_to_Pressure(vol);
 			printf("检测重量为：%.2fkg\r\n", pressure);
+			
+			sprintf(buffer, "AD: %d, vol: %.2fV, weight: %.2fkg\r\n", value, vol, pressure);
+			f_puts(buffer, &fsrc);
 		}
 		delay_ms(10);
-		*/
 		
 	}
 }
